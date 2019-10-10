0ba03bea14cfbafedba26c954c3c107ef29304ba
niklas.langner <niklas.langner@rwth-aachen.de>
2019-10-10 17:49:32 +0200

    [edgeconv] Add k-nearest-neighbors function

--- edgeconv.py
+++ edgeconv.py
@@ -1,6 +1,8 @@
 """Implementation of EdgeConv using arbitrary functions as h"""
+import tensorflow as tf
 import tensorflow.keras.layers as lay
 from tensorflow import keras
+from tensorflow.keras import backend as K
 
 
 class EdgeConv(lay.Layer):
@@ -20,3 +22,12 @@ class EdgeConv(lay.Layer):
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0], 30)
+
+
+def knn(X, k):
+    r = K.sum(X * X, axis=1, keepdims=True)  # (N, 1, P_A)
+    m = K.batch_dot(K.permute_dimensions(K.transpose(X), (2, 0, 1)), X)  # (N, P_A, P_B)
+    D = lay.add([lay.subtract([K.permute_dimensions(K.transpose(r), (2, 0, 1)), 2 * m]), r])
+    values, indices = tf.nn.top_k(-D, k=k+1)  # (N, P, K+1)
+    indices = K.slice(indices, [0, 0, 1], [X.shape[0], X.shape[2], k])  # (N, P, K)
+    return indices
